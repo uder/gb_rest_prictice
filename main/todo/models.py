96d399d543cdc90a6ccd784f32dddcb5364d8b26
PavelS <uder.tab@gmail.com>
2021-03-08 10:18:41 +1000

    Create Project and ToDo models

--- main/todo/models.py
+++ main/todo/models.py
@@ -8,3 +8,18 @@ class User(models.Model):
     first_name=models.CharField(max_length=64)
     last_name=models.CharField(max_length=64)
     email=models.CharField(max_length=255, unique=True)
+
+class Project(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=64, unique=True)
+    repo_url=models.URLField()
+    users=models.ManyToManyField(User)
+
+class ToDo(models.Model):
+    uuid=models.UUIDField(primary_key=True, default=uuid4)
+    project=models.ForeignKey(Project,on_delete=models.CASCADE)
+    text=models.CharField(max_length=32767)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    active=models.BooleanField()
+
