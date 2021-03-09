43bc09b9696dd2cc2b3a10dd8d8fa0a471934a3d
PavelS <uder.tab@gmail.com>
2021-03-09 20:07:20 +1000

    Fix Project and ToDo models

--- main/todo/models.py
+++ main/todo/models.py
@@ -9,11 +9,17 @@ class User(models.Model):
     last_name=models.CharField(max_length=64)
     email=models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.user_name
+
 class Project(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=64, unique=True)
     repo_url=models.URLField()
     users=models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
 
 class ToDo(models.Model):
     uuid=models.UUIDField(primary_key=True, default=uuid4)
@@ -21,5 +27,6 @@ class ToDo(models.Model):
     text=models.CharField(max_length=32767)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    user_creator=models.ForeignKey(User, on_delete=models.SET_DEFAULT,default=0)
     active=models.BooleanField()
 
