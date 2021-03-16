cbc9940dddd8db86dd48d758c375d7a4b282cf9d
PavelS <uder.tab@gmail.com>
2021-03-16 12:49:26 +1000

    ToDoModel pagination implementation. Redefine destory method in Viewset

--- main/todo/pagination.py
+++ main/todo/pagination.py
@@ -2,3 +2,6 @@ from rest_framework.pagination import LimitOffsetPagination
 
 class ProjectPagination(LimitOffsetPagination):
     default_limit = 10
+
+class ToDoPagination(LimitOffsetPagination):
+    default_limit = 20
--- main/todo/views.py
+++ main/todo/views.py
@@ -1,12 +1,12 @@
 # from django.shortcuts import render
-# from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import ListModelMixin,RetrieveModelMixin, UpdateModelMixin
-# from rest_framework.response import Response
+from rest_framework.response import Response
 from .models import User,Project,ToDo
 from .serializers import UserModelSreializer,ProjectModelSerializer,ToDoModelSerializer
 from .filters import ProjectFilter
-from .pagination import ProjectPagination
+from .pagination import ProjectPagination, ToDoPagination
 
 # Create your views here.
 class UserModelViewSet(ModelViewSet):
@@ -22,7 +22,15 @@ class ProjectModelViewSet(ModelViewSet):
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+    pagination_class = ToDoPagination
+
+    def destroy(self,request, pk=None):
+        todo_record=get_object_or_404(ToDo,pk=pk)
+        todo_record.active=False
+        todo_record.save()
+        return Response({f"{todo_record}.Active": False})
 
 class UserGenericViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin):
     queryset = User.objects.all()
     serializer_class = UserModelSreializer
+
