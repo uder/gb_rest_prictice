8e922eb3e13eaad6a7d5524a3a9bb6f4a1de8596
PavelS <uder.tab@gmail.com>
2021-03-25 13:00:03 +1000

    Basic Auth Implemented

--- main/main/settings.py
+++ main/main/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'django_filters',
+    # 'rest_framework.authtoken',
 
     'todo',
 ]
@@ -155,5 +156,14 @@ REST_FRAMEWORK={
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
 
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.DjangoModelPermissions',
+    ]
+
 }
