d308798b1766316b2ad098d3a41b1d391d7ccff8
PavelS <uder.tab@gmail.com>
2021-04-01 21:29:41 +1000

    Tests for ProjectView and TODOView using ApiClient and APITestCase

--- main/todo/tests.py
+++ main/todo/tests.py
@@ -1,13 +1,11 @@
-import json
 from django.test import TestCase
 from django.contrib.auth.models import User as DjangoUser
 from rest_framework.test import APITestCase,APIClient,APIRequestFactory,force_authenticate
 from rest_framework import status
 from mixer.backend.django import mixer
-from uuid import uuid4
 
-from .views import UserGenericViewSet,UserModelViewSet
-from .models import Project,User as TODOUser,ToDo
+from .views import UserGenericViewSet
+from .models import Project,User,ToDo
 # Create your tests here.
 
 
@@ -29,7 +27,7 @@ class TestUserView(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_user_retrive(self):
-        retrive_user=mixer.blend(TODOUser)
+        retrive_user=mixer.blend(User)
         factory=APIRequestFactory()
         request=factory.get(f'/api/user/{retrive_user.uuid}')
         force_authenticate(request,self.admin)
@@ -40,7 +38,7 @@ class TestUserView(TestCase):
 
     def test_user_update(self):
         factory=APIRequestFactory()
-        user=mixer.blend(TODOUser)
+        user=mixer.blend(User)
         request=factory.patch(f'/api/user/{user.uuid}/', {'email':'newmail@example.com'}, format='json')
         force_authenticate(request,self.admin)
         view=UserGenericViewSet.as_view({'patch':'partial_update'})
@@ -49,28 +47,83 @@ class TestUserView(TestCase):
         self.assertEqual(response.data.get('email'), 'newmail@example.com')
 
 
+class TestProjectView(TestCase):
+    def setUp(self):
+        self.admin_name='admin'
+        self.admin_pass='123456'
+        admin_mail='admin@example.com'
+        self.admin=DjangoUser.objects.create_superuser(self.admin_name,admin_mail,self.admin_pass)
+
+    def test_projects_list(self):
+        client=APIClient()
+        client.force_authenticate(user=self.admin)
+        response=client.get('/api/project/')
+        client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_project_create(self):
+        client=APIClient()
+        client.force_authenticate(user=self.admin)
+        response=client.post('/api/project/', {'name': 'DRF_TEST_project', 'repo_url': 'http://example.com/test'})
+        client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_project_retrive(self):
+        project=mixer.blend(Project)
+        client=APIClient()
+        client.force_authenticate(user=self.admin)
+        response=client.get(f'/api/project/{project.id}/')
+        client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('name'), project.name)
+
+    def test_project_update(self):
+        project=mixer.blend(Project)
+        client=APIClient()
+        client.force_authenticate(user=self.admin)
+        response=client.patch(f'/api/project/{project.id}/',{'name':'DRF_TEST_NewName'})
+        client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('name'), 'DRF_TEST_NewName')
+
+class TestToDoView(APITestCase):
+    def setUp(self):
+        self.admin_name='admin'
+        self.admin_pass='123456'
+        admin_mail='admin@example.com'
+        self.admin=DjangoUser.objects.create_superuser(self.admin_name,admin_mail,self.admin_pass)
 
 
+    def test_todo_list(self):
+        self.client.force_authenticate(user=self.admin)
+        response=self.client.get('/api/todo/')
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-# class TestProjectView(TestCase):
-#     def setUp(self):
-#         self.users=[]
-#         for i in range(1,4):
-#             name=f'DRF_test{str(i)}'
-#             user=mixer.blend(TODOUser,user_name=name)
-#             self.users.append(user)
-#             user.save()
-#
-#         self.admin_name='admin'
-#         self.admin_pass='123456'
-#         admin_mail='admin@example.com'
-#         self.admin=DjangoUser.objects.create_superuser(self.admin_name,admin_mail,self.admin_pass)
-#
-#     def test_create_project(self):
-#         client=APIClient()
-#         project=Project(id=100, name='Test_project', repo_url='repo@example.com')
-#         # project.users.set(self.users)
-#         # project.save()
-#         client.login(username=self.admin_name,passwd=self.admin_pass)
-#         response=client.post('/api/project/',project)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_todo_create(self):
+        project=mixer.blend(Project)
+        user=mixer.blend(User)
+        self.client.force_authenticate(user=self.admin)
+        response=self.client.post('/api/todo/', {'project':project.id, 'userCreator':user.uuid,'text':'DRF_TEST_Text'})
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_todo_retrieve(self):
+        project=mixer.blend(Project)
+        user=mixer.blend(User)
+        todo=mixer.blend(ToDo, user_creator=user, project=project)
+        self.client.force_authenticate(user=self.admin)
+        response=self.client.get(f'/api/todo/{todo.uuid}/')
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('text'), todo.text)
+
+    def test_todo_update(self):
+        project=mixer.blend(Project)
+        user=mixer.blend(User)
+        todo=mixer.blend(ToDo, user_creator=user, project=project)
+        self.client.force_authenticate(user=self.admin)
+        response=self.client.patch(f'/api/todo/{todo.uuid}/',{'text':'DRF_TEST_NewText'})
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('text'), 'DRF_TEST_NewText')
