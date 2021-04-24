706a2a36414402570fc17b7507a232ae22e8402d
PavelS <uder.tab@gmail.com>
2021-04-24 15:37:17 +1000

    fix. add text field to todo api

--- main/todo/serializers.py
+++ main/todo/serializers.py
@@ -34,4 +34,4 @@ class ToDoModelSerializer(HyperlinkedModelSerializer):
     user_creator=PrimaryKeyRelatedField(queryset=User.objects.all())
     class Meta:
         model=ToDo
-        fields=['url','uuid','project','created_at','updated_at','user_creator','active']
+        fields=['url','uuid','project','text','created_at','updated_at','user_creator','active']
